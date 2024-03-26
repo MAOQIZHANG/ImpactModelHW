ec9907ef301a25f207ae203f3454f30cfcc86918
jycathy <jc11552@nyu.edu>
2024-03-26 17:08:55 -0400

    modify impact calculation for test

--- utils/impact_calculation.py
+++ utils/impact_calculation.py
@@ -1,23 +1,35 @@
 import pandas as pd
 import os
 
-with open('Impact-Model-Matrix/vwap_end_55800000.pkl', 'rb') as f:
-    VWAP_330 = pd.read_pickle(f)
+class ImpactCalculation():
 
-with open('Impact-Model-Matrix/ArrivalPrice.pkl', 'rb') as f:
-    Arrival = pd.read_pickle(f)
+    def __init__(
+            self, 
+        ):
 
-with open('Impact-Model-Matrix/TerminalPrice.pkl', 'rb') as f:
-    Terminal = pd.read_pickle(f)
+        with open('Impact-Model-Matrix/vwap_end_55800000.pkl', 'rb') as f:
+            self.VWAP_330 = pd.read_pickle(f)
 
-# permanent impact g
-g = (Terminal - Arrival)/2
-# temporary impact h
-h = VWAP_330 - Arrival - g
+        with open('Impact-Model-Matrix/ArrivalPrice.pkl', 'rb') as f:
+            self.Arrival = pd.read_pickle(f)
 
-output_path = 'Impact-Model-Matrix/TemporaryImpact.pkl'
-if not os.path.exists(output_path):
-    h.to_pickle(output_path)
-    print("Finish calculating impacts")
-else:
-    print("Impact file exists")
+        with open('Impact-Model-Matrix/TerminalPrice.pkl', 'rb') as f:
+            self.Terminal = pd.read_pickle(f)
+
+    def compute_h(self):
+        # permanent impact g
+        self.g = (self.Terminal - self.Arrival)/2
+        # temporary impact h
+        self.h = self.VWAP_330 - self.Arrival - self.g
+
+        output_path = 'Impact-Model-Matrix/TemporaryImpact.pkl'
+        if not os.path.exists(output_path):
+            self.h.to_pickle(output_path)
+            print("Finish calculating impacts")
+        else:
+            print("Impact file exists")
+
+
+if __name__ == "__main__":
+    impact_cal = ImpactCalculation()
+    impact_cal.compute_h()
