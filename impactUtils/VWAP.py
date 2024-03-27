eea6ec5062dcf2aa438deb22ab5efd85778e3172
Yunlei (Louis) Lu <luyunlei99@gmail.com>
2024-03-27 01:20:30 -0400

    Update VWAP.py

--- impactUtils/VWAP.py
+++ impactUtils/VWAP.py
@@ -1,29 +1,31 @@
-# Class to calculate volume weighted average
-# price for a given day of data between
-# start timestamp and end timesamp,
-# exclusive
+# Class to calculate volume weighted average price for a given day of data between start timestamp and end timesamp, exclusive
 class VWAP(object):
     def __init__(self, data, startTS, endTS):
-        v = 0
-        s = 0
+        self.v = 0
+        self.s = 0
         counter = 0
+
         for i in range(0, data.getN()):
             if data.getTimestamp(i) < startTS:
                 continue
             if data.getTimestamp(i) >= endTS:
                 break
             counter = counter + 1
-            v = v + (data.getSize(i) * data.getPrice(i))
-            s = s + data.getSize(i)
+            self.v += (data.getSize(i) * data.getPrice(i))
+            self.s += data.getSize(i)
+
         if counter == 0:
-            self.counter = 0
-            self.vwap = None
+            self._counter = 0
+            self._vwap = None
         else:
             self._counter = counter
-            self._vwap = v / s
+            self._vwap = self.v / self.s
 
     def getVWAP(self):
         return self._vwap
+    
+    def getValue(self):
+        return self.v
 
     def getN(self):
         return self._counter
